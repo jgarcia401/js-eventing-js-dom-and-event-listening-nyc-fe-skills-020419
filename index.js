084a85aff39f5ed711346908370d90d731f83193
resourceful-transpiler-3397 <jgarcia401@gmail.com>
2019-03-25 23:39:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,3 +58,17 @@ button.addEventListener('click', () => {
   
 })
 
+const divs = document.querySelectorAll('div')
+
+// [div1, div2, div3, div4, div5]
+
+for(let div of divs) {
+  
+  div.addEventListener('click', (event) => {
+    
+    console.log(event)
+  })
+}
+
+
+
